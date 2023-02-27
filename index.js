3fe1ebb8d9453a7dd072818d17f0dff0c3f3e299
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-02-27 22:53:32 +0800

    make it better

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ app.use(AuthRoute);
 
 // store.sync(); //memambahkan session ke database, nonaktifkan setelah berhasil menambahkan
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server up and running...");
 });
