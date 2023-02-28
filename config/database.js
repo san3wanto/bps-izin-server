e70ce9c31615084849c812db55ba476f97419cbb
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-02-28 08:41:24 +0800

    commit heroku

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("sizinbps", "root", "", {
-  host: "127.0.0.1",
+const db = new Sequelize("freedb_sizinbps", "freedb_usersizin", "FJ%pwUV4wn@9!Xv", {
+  host: "sql.freedb.tech",
   dialect: "mysql",
 });
+// const db = new Sequelize("sizinbps", "root", "", {
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+// });
 
 // jangan goblok
 
