e18a6fc7a4f491b11b7fe0b2f35ee0016a9798e0
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-02-28 15:16:00 +0800

    ini jangan kore

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,17 +4,17 @@ import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get("/users", getUser);
-router.get("/users/:id", getUserById);
-router.post("/users", createUser);
-router.patch("/users/:id", updateUser);
-router.delete("/users/:id", deleteUser);
+// router.get("/users", getUser);
+// router.get("/users/:id", getUserById);
+// router.post("/users", createUser);
+// router.patch("/users/:id", updateUser);
+// router.delete("/users/:id", deleteUser);
 
-// router.get("/users", verifyUser, getUser);
-// router.get("/users/:id", verifyUser, getUserById);
-// router.post("/users", verifyUser, adminOnly, createUser);
-// router.patch("/users/:id", verifyUser, updateUser);
-// router.patch("/users/:id/status", verifyUser, updateUserStatus);
-// router.delete("/users/:id", verifyUser, adminOnly, deleteUser);
+router.get("/users", verifyUser, getUser);
+router.get("/users/:id", verifyUser, getUserById);
+router.post("/users", verifyUser, adminOnly, createUser);
+router.patch("/users/:id", verifyUser, updateUser);
+router.patch("/users/:id/status", verifyUser, updateUserStatus);
+router.delete("/users/:id", verifyUser, adminOnly, deleteUser);
 
 export default router;
